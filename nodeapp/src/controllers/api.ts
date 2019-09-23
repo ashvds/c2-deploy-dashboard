e1cd9ed132f1b51f9df6898ed3855836cc5a2685
ashvds <ashvds@gmail.com>
2019-09-23 12:03:57 +1000

    more express stuff

--- nodeapp/src/controllers/api.ts
+++ nodeapp/src/controllers/api.ts
@@ -1,6 +1,5 @@
 "use strict";
 
-import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 
 
@@ -14,18 +13,4 @@ export const getApi = (req: Request, res: Response) => {
     });
 };
 
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-    graph.setAccessToken(token.accessToken);
-    graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-        if (err) { return next(err); }
-        res.render("api/facebook", {
-            title: "Facebook API",
-            profile: results
-        });
-    });
-};
+
